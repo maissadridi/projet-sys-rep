67ec01728e4097bf3d1a74394d67adc6b4c83142
Mariem BenYounes <maryemby21@gmail.com>
2024-03-26 21:52:53 +0100

    added tests

--- src/main/java/rmi/TaskListClient.java
+++ src/main/java/rmi/TaskListClient.java
@@ -6,7 +6,12 @@ public class TaskListClient {
     public static void main(String[] args) {
         try {
             TaskListInterface obj = (TaskListInterface) Naming.lookup("//localhost/TaskListServer");
-            // Utilisez les méthodes de l'interface TaskListInterface ici
+            //test
+            obj.addTask("Task 1");
+            obj.addTask("Task 2");
+            obj.addTask("Task 3");
+            obj.removeTask(1);
+            obj.getTasks().forEach(System.out::println);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
