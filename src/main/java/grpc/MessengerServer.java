e192f9dccaf2884c7bb6cbba4c868ffb5bfa6688
Mariem BenYounes <maryemby21@gmail.com>
2024-03-26 22:38:07 +0100

    removed implementation

--- src/main/java/grpc/MessengerServer.java
+++ src/main/java/grpc/MessengerServer.java
@@ -1,8 +1,6 @@
 package grpc;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
-import io.grpc.stub.StreamObserver;
-import grpc.MessengerGrpc.MessengerImplBase;
 
 public class MessengerServer {
     private int port = 50051;
@@ -42,27 +40,5 @@ public class MessengerServer {
         server.blockUntilShutdown();
     }
 
-    static class MessengerImpl extends grpc.MessengerImplBase {
-        @Override
-        public void sendMessage(MessageRequest request, StreamObserver<MessageResponse> responseObserver) {
-            // Implémentez la logique de l'envoi de message ici
-            String status = "Message sent successfully to " + request.getRecipient();
-            MessageResponse response = MessageResponse.newBuilder().setStatus(status).build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        }
-
-        @Override
-        public void getMessagesForUser(UserRequest request, StreamObserver<MessageList> responseObserver) {
-            // Implémentez la logique de récupération des messages ici
-            String username = request.getUsername();
-            MessageList messageList = MessageList.newBuilder()
-                    .addMessages("Message 1 for " + username)
-                    .addMessages("Message 2 for " + username)
-                    .build();
-            responseObserver.onNext(messageList);
-            responseObserver.onCompleted();
-        }
-    }
 }
 
